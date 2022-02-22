759543526e5b0099dd642994d2c18b47274d1e71
JoJ3o <42371079+JoJ3o@users.noreply.github.com>
2022-02-22 13:11:17 +0100

    Install default eslint file

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,35 +1,12 @@
-// The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
-const functions = require('firebase-functions');
+import express, { Request, Response } from 'express';
 
-// The Firebase Admin SDK to access Firestore.
-const admin = require('firebase-admin');
-admin.initializeApp();
+const app = express();
+const port = 3000;
 
-// Take the text parameter passed to this HTTP endpoint and insert it into
-// Firestore under the path /messages/:documentId/original
-exports.addMessage = functions.https.onRequest(async (req: any, res: any) => {
-	// Grab the text parameter.
-	const original = req.query.text;
-	// Push the new message into Firestore using the Firebase Admin SDK.
-	const writeResult = await admin.firestore().collection('messages').add({original: original});
-	// Send back a message that we've successfully written the message
-	res.json({result: `Message with ID: ${writeResult.id} added.`});
+app.get('/', (_req: Request, res: Response) => {
+  res.send('Hello World!');
 });
 
-// Listens for new messages added to /messages/:documentId/original and creates an
-// uppercase version of the message to /messages/:documentId/uppercase
-exports.makeUppercase = functions.firestore.document('/messages/{documentId}')
-	.onCreate((snap: any, context: any) => {
-		// Grab the current value of what was written to Firestore.
-		const original = snap.data().original;
-
-		// Access the parameter `{documentId}` with `context.params`
-		functions.logger.log('Uppercasing', context.params.documentId, original);
-
-		const uppercase = original.toUpperCase();
-
-		// You must return a Promise when performing asynchronous tasks inside a Functions such as
-		// writing to Firestore.
-		// Setting an 'uppercase' field in Firestore document returns a Promise.
-		return snap.ref.set({uppercase}, {merge: true});
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
 });
