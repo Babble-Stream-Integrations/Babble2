44e1a8c02105b67a22b60d17ae1fc3ded4f3b476
FinndeJong <finn.dejong@student.hu.nl>
2022-03-28 17:53:29 +0200

    overzetten naar vaste pc

--- src/components/prototype/PrototypeComponent.tsx
+++ src/components/prototype/PrototypeComponent.tsx
@@ -7,10 +7,18 @@ function PrototypeComponent() {
     <div className="PC-container">
       <div className="PC-settings-container">
         <div className="PC-grid">
+          <div>
+            <button className="PC-button">Auth Youtube</button>
+          </div>
+          <button className="PC-button">Auth Twitch</button>
           <input type="input" />
           <input type="input" />
           <input type="input" />
           <input type="input" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
+          <input type="checkbox" />
         </div>
       </div>
     </div>
