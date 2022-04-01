d17dc9d1d98165dd74cf818bb89b26e25fd85580
Hanne <hanne.dedeckere@student.hu.nl>
2022-04-01 10:58:20 +0200

    Puntjes op de i

--- src/components/Navbar/Header.js
+++ src/components/Navbar/Header.js
@@ -2,32 +2,32 @@ import { useMatchMedia } from "../../helpers/useMatchMedia";
 import logo from "../../assets/logo/Babble-Small-Transparant.png";
 import logo2 from "../../assets/logo/Babble-Big-Transparant.png";
 import avatar from "../../assets/avatar.png";
-import { HiMenuAlt4 } from "react-icons/hi";
+import hamburger from "../../assets/hamburgericon.png";
 import "./Header.css";
 
 export const Header = () => {
-  const isDesktop = useMatchMedia("(min-width: 969px)", true);
-  const isPhone = useMatchMedia("(max-width: 968px)", true);
+  const isDesktop = useMatchMedia("(min-width: 500px)", true);
+  const isPhone = useMatchMedia("(max-width: 499px)", true);
 
   return (
-    <div className="container">
+    <div className="menubalk">
       {isPhone && <img className="logo" alt="Babble" src={logo} />}
       {isDesktop && <img className="logo" alt="Babble" src={logo2} />}
       {isPhone && (
         <div className="menu">
-          <HiMenuAlt4 />
+          <img className="logo" alt="Babble" src={hamburger} />
         </div>
       )}
       {isDesktop && (
         <div className="header-flex">
           <div className="avatar">
-            <img className="profile" src={avatar} alt="profielfoto" />
+            <img className="profile" src={avatar} alt="profile" />
           </div>
           <div className="button">
-            <button>Log uit</button>
+            <button>Log out</button>
           </div>
           <div className="menu">
-            <HiMenuAlt4 />
+            <img className="hamburger" alt="Babble" src={hamburger} />
           </div>
         </div>
       )}
