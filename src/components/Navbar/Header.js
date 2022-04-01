a415b9e0b9d324cad77726a84b29e48185311e75
Hanne <hanne.dedeckere@student.hu.nl>
2022-04-01 11:23:20 +0200

    Opmerkingen Finn toegepast

--- src/components/Navbar/Header.js
+++ src/components/Navbar/Header.js
@@ -11,23 +11,23 @@ export const Header = () => {
 
   return (
     <div className="menubalk">
-      {isPhone && <img className="logo" alt="Babble" src={logo} />}
-      {isDesktop && <img className="logo" alt="Babble" src={logo2} />}
+      {isPhone && <img className="babble-logo" alt="Babble" src={logo} />}
+      {isDesktop && <img className="babble-logo" alt="Babble" src={logo2} />}
       {isPhone && (
-        <div className="menu">
-          <img className="logo" alt="Babble" src={hamburger} />
+        <div className="nav-menu">
+          <img className="hamburger-menu" alt="Babble" src={hamburger} />
         </div>
       )}
       {isDesktop && (
         <div className="header-flex">
-          <div className="avatar">
-            <img className="profile" src={avatar} alt="profile" />
+          <div className="nav-avatar">
+            <img className="nav-avatar-profile" src={avatar} alt="profile" />
           </div>
-          <div className="button">
-            <button>Log out</button>
+          <div>
+            <button className="log-button">Log out</button>
           </div>
-          <div className="menu">
-            <img className="hamburger" alt="Babble" src={hamburger} />
+          <div className="nav-menu">
+            <img className="hamburger-menu" alt="Babble" src={hamburger} />
           </div>
         </div>
       )}
