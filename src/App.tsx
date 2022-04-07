175ce5004eab34c1dcec4afe140b3eef23792ac0
FinndeJong <70587533+FinndeJong@users.noreply.github.com>
2022-04-07 09:56:22 +0200

    main page created

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Prototype from "./pages/prototype/Prototype";
+import Main_Page from "./pages/main_page/Main_Page";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        {/* <Route path="/" element={} /> */}
+        <Route path="/" element={<Main_Page />} />
         <Route path="/prototype" element={<Prototype />} />
       </Routes>
     </div>
