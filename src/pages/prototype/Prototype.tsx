6e77b265eeb4b5d8455f9159c5a6cdfaa84a22e9
FinndeJong <70587533+FinndeJong@users.noreply.github.com>
2022-04-21 14:14:15 +0200

    PUT fetch genereren tokens FE

--- src/pages/prototype/Prototype.tsx
+++ src/pages/prototype/Prototype.tsx
@@ -13,6 +13,35 @@ function Prototype() {
       setPmodalshow(false);
       console.log("testtest");
     }
+
+    const params = window.location.search;
+    if (params.trim().length !== 0) {
+      const codeParam = new URLSearchParams(params).get("code");
+      fetch(
+        `http://localhost:5001/babble-d6ef3/europe-west1/default/api/v1/users/${localStorage.getItem(
+          "UUID"
+        )}/tokens/twitch`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            code: codeParam,
+          }),
+        }
+      )
+        .then((response) =>
+          response.json().then((data) => {
+            console.log(data);
+            alert("login succesfull");
+          })
+        )
+        .catch((err) => {
+          console.log("Error Reading data " + err);
+          alert("error during login try again");
+        });
+    }
   }, []);
   return (
     <div className="prototype-container">
