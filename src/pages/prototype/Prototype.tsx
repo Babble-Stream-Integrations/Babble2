7a999ca2cabc75199322950b524d1fd5477321ff
FinndeJong <70587533+FinndeJong@users.noreply.github.com>
2022-04-21 14:15:56 +0200

    URL fix

--- src/pages/prototype/Prototype.tsx
+++ src/pages/prototype/Prototype.tsx
@@ -3,6 +3,16 @@ import { useState, useEffect } from "react";
 import PrototypeComponent from "../../components/prototype/PrototypeComponent";
 import Prototype_modal from "../../components/prototype_modal/Prototype_modal";
 
+const uuid = localStorage.getItem("UUID");
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "https://europe-west1-babble-d6ef3.cloudfunctions.net/default"
+    : "http://localhost:5001/babble-d6ef3/europe-west1/default";
+const origin =
+  process.env.NODE_ENV === "production"
+    ? "https://dev-babble.web.app"
+    : "http://localhost:3000";
+
 function Prototype() {
   const [Pmodalshow, setPmodalshow] = useState(true);
   useEffect(() => {
@@ -17,20 +27,15 @@ function Prototype() {
     const params = window.location.search;
     if (params.trim().length !== 0) {
       const codeParam = new URLSearchParams(params).get("code");
-      fetch(
-        `http://localhost:5001/babble-d6ef3/europe-west1/default/api/v1/users/${localStorage.getItem(
-          "UUID"
-        )}/tokens/twitch`,
-        {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            code: codeParam,
-          }),
-        }
-      )
+      fetch(`${baseURL}/api/v1/users/${uuid}/tokens/twitch`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          code: codeParam,
+        }),
+      })
         .then((response) =>
           response.json().then((data) => {
             console.log(data);
