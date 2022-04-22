b1838db286e85cdb5e97aafd7752ab1c54dbcb15
kaischreurs <70720881+kaischreurs@users.noreply.github.com>
2022-04-22 15:30:31 +0200

    vrijdag

--- functions/src/routes/addonHost.ts
+++ functions/src/routes/addonHost.ts
@@ -1,22 +1,21 @@
-import express from "express";
+import express, { Request } from "express";
 import path from "path";
 // import getFirestore from "firebase-admin/firestore";
-import { Query } from "express-serve-static-core";
 // const db = getFirestore();
 import Sse from "../services/serverSentEvents";
 
-export interface TypedRequestQuery<T extends Query> extends Express.Request {
-    on: any;
-    query: T;
-}
-
 const router = express.Router();
 
-router.get('/raffle', async (req, res) => {res.sendFile(path.join(__dirname, '/index.html'))});
-router.get('/raffle/listen', async (req: TypedRequestQuery<{id:string}>, res) => {
-	const id = req.query.id;
-	Sse.connect(id, res);
-	req.on("close", Sse.disconnect.bind(id));
+router.get("/raffle", async (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
+router.get(
+  "/raffle/listen",
+  async (req: Request<unknown, unknown, unknown, { id: string }>, res) => {
+    const { id } = req.query;
+    Sse.connect(id, res);
+    req.on("close", Sse.disconnect.bind(id));
+  }
+);
 
 export default router;
--- functions/src/services/realtimeDB.ts
+++ functions/src/services/realtimeDB.ts
@@ -1,4 +1,5 @@
-import { getDatabase } from "firebase/database";
+import { getDatabase, ref, set } from "firebase/database";
+
 const realtimeDB = getDatabase(
   "https://babble-d6ef3-default-rtdb.europe-west1.firebasedatabase.app"
 );
