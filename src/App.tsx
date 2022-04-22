297bc5499c878b2511748c7d71a24ac31b14749f
JoJ3o <42371079+JoJ3o@users.noreply.github.com>
2022-04-22 10:24:33 +0200

    Prototype (#8)
    * update 5
    
    * overzetten naar vaste pc
    
    * Grote switch component
    
    * Lelijk maar puur voor de werking
    
    * update modal uuid
    
    * update 6
    
    * api test
    
    * Back-end is workable now pogpog
    
    * React + Node proxy connection
    
    * Setting header
    
    * Firebase deploy
    
    * Import TS files
    
    * Convert all user routes
    
    * Commit for pull
    
    * UUID inlog/verification works
    
    * Auth routes
    
    * delete lib
    
    * Update FE, huidig niet werkend
    
    * Twitch Auth && Save data
    
    * fix header error
    
    * Auto log-in if already logged in
    
    * Spelling fix
    
    * Implement raffle addon route
    
    * fix #10
    
    * Fix addon route
    
    * Quick fix #4
    
    * FE fully workend
    
    * twitchChat connect client
    
    * getStatus & calculateUsersEntered
    
    * Fix entire addon + Testing
    
    * main page created
    
    * rewrite hosting
    
    * console logs removed & alert fix
    
    * input validation
    
    prevent inputs from having nothing in it, preven number inputs from having lower than 1 or 60 in it. Automatically change them if these numbers are lower
    
    * some standard values
    
    * invalid accessToken & refreshToken handling
    
    * change alert
    
    * lil fixes
    
    * Deploy ready
    
    * cors & copyDoc
    
    * generate auth Token & and save tokens dynamically
    
    * Solve ESLint errors part 1
    
    * fix axios error & functions linter
    
    * Refactor pickWinner function
    
    * dynamic env fetches
    
    dynamically change fetches according to environment
    
    * host target to dev
    
    * divide dev and main deploy scripts
    
    * correct scope checking
    
    * Revert "correct scope checking"
    
    This reverts commit b1fc5264e5100c461108c9f57e6aa6c028082f37.
    
    * PUT fetch genereren tokens FE
    
    * URL fix
    
    * redirect backend to dev
    
    * change names
    
    * raffle hotfix
    
    * change my own file names
    
    * import fix
    
    Co-authored-by: FinndeJong <finn.dejong@student.hu.nl>
    Co-authored-by: FinndeJong <70587533+FinndeJong@users.noreply.github.com>
    Co-authored-by: Hanne <hanne.dedeckere@student.hu.nl>

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import AddonPage from "./pages/addon_page/AddonPage";
+import Prototype from "./pages/prototype/Prototype";
+import MainPage from "./pages/mainPage/MainPage";
+import AddonPage from "./pages/addonPage/AddonPage";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<AddonPage />} />
+        <Route path="/" element={<MainPage />} />
+        <Route path="/prototype" element={<Prototype />} />
+        <Route path="/addons" element={<AddonPage />} />
       </Routes>
     </div>
   );
