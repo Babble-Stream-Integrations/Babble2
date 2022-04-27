53da2fa0ff2613a597241e66c292b39da085086d
kaischreurs <70720881+kaischreurs@users.noreply.github.com>
2022-04-27 23:45:01 +0200

    rtdb werkt, alleen nog laatste dingen doen.

--- functions/src/routes/addonHost.ts
+++ functions/src/routes/addonHost.ts
@@ -3,6 +3,7 @@ import path from "path";
 // import getFirestore from "firebase-admin/firestore";
 // const db = getFirestore();
 import Sse from "../services/serverSentEvents";
+import RTDB from "../services/realtimeDB";
 
 const router = express.Router();
 
@@ -12,9 +13,10 @@ router.get("/raffle", async (req, res) => {
 router.get(
   "/raffle/listen",
   async (req: Request<unknown, unknown, unknown, { id: string }>, res) => {
-    const { id } = req.query;
-    Sse.connect(id, res);
-    req.on("close", Sse.disconnect.bind(id));
+    // const { id } = req.query;
+    // Sse.connect(id, res);
+    // req.on("close", Sse.disconnect.bind(id));
+	RTDB.start();
   }
 );
 
--- functions/src/services/realtimeDB.ts
+++ functions/src/services/realtimeDB.ts
@@ -1,5 +1,23 @@
-import { getDatabase, ref, set } from "firebase/database";
+import firebase from "firebase-admin"
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 
-const realtimeDB = getDatabase(
-  "https://babble-d6ef3-default-rtdb.europe-west1.firebasedatabase.app"
-);
+const RTDBApp = initializeApp({databaseURL: "https://babble-d6ef3-default-rtdb.europe-west1.firebasedatabase.app"}, 'RTDBApp');
+
+const realtimeDB = getDatabase(RTDBApp);
+const DBref = firebase.database().ref();
+const test3 = 'testtesttest'
+// input functie id: string, duration: bigint
+async function start() {
+	DBref.child(test3).set({
+			werkend:'true',
+			errors:'hopelijk niet'
+	});
+}
+
+export default {
+	// connect,
+	// disconnect,
+	start,
+	// end,
+};
