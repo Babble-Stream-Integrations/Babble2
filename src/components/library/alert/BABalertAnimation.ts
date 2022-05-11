c0318dc0d152cdd3c6e5fc358c8c09808433c8e3
JoJ3o <42371079+JoJ3o@users.noreply.github.com>
2022-05-11 10:07:40 +0200

    #bab 238 streamer visual 2 (#13)
    * Merge met vaste pc
    
    * Herbruikbaar Alert component
    
    * Visual klaar
    
    alleen animation
    
    * package vulnerability fix and alert animation
    
    * Alert aan de goede knop gehangen, Tijdelijke kleuren veranderd
    
    * transitions voor alle plekken
    
    * update presentatie ready
    
    * Update src/components/library/alert/BABalertAnimation.ts
    
    * Comments en ;
    
    * clear up code, lint command update
    
    Co-authored-by: FinndeJong <finn.dejong@student.hu.nl>
    Co-authored-by: FinndeJong <70587533+FinndeJong@users.noreply.github.com>

--- src/components/library/alert/BABalertAnimation.ts
+++ src/components/library/alert/BABalertAnimation.ts
@@ -1,28 +1,8 @@
 export function ChooseAlertAnimation(position: string) {
-  if (
-    position === "BottomLeft" ||
-    position === "TopLeft" ||
-    position === "MiddleLeft"
-  ) {
-    console.log(`"BottomLeft" || "TopLeft" || "MiddleLeft"`);
-    return "alert-animation-left";
-  } else if (
-    position === "BottomRight" ||
-    position === "TopRight" ||
-    position === "MiddleRight"
-  ) {
-    console.log(`"BottomRight" || "TopRight" || "MiddleRight"`);
-    return "alert-animation-right";
-  } else if (position === "TopMiddle") {
-    console.log("TopMiddle");
-    return "alert-animation-top";
-  } else if (position === "BottomMiddle") {
-    console.log("BottomMiddle");
-    return "alert-animation-bottom";
-  } else if (position === "Center") {
-    console.log("Center");
-    return "alert-animation-center";
-  } else {
-    return "";
-  }
+  if (position.match(/Left/g)) return "alert-animation-left";
+  if (position.match(/Right/g)) return "alert-animation-right";
+  if (position.match(/Top/g)) return "alert-animation-top";
+  if (position.match(/Bottom/g)) return "alert-animation-bottom";
+  if (position.match(/Center/g)) return "alert-animation-center";
+  return "";
 }
