355902cd5440359b325efe8457f5f977fb3915e4
kaischreurs <70720881+kaischreurs@users.noreply.github.com>
2022-05-12 13:59:41 +0200

    Update functions/src/index.ts
    change route of addonhosting
    
    Co-authored-by: JoJ3o <42371079+JoJ3o@users.noreply.github.com>

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
 app.use("/api/v1", userRoutes);
 app.use("/api/v1", addonRoutes);
 app.use("/api/v1", authRoutes);
-app.use("/api/v1", hostRoutes);
+app.use("/overlay", hostRoutes);
 
 app.get("/", (_req: Request, res: Response) => {
   res.send({
