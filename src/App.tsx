50f0c4cf726184db97f6184f60d2a805e7227d30
FinndeJong <70587533+FinndeJong@users.noreply.github.com>
2022-05-23 23:45:18 +0200

    logo update, invalidlink page with return

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Prototype from "./pages/prototype/Prototype";
 import MainPage from "./pages/mainPage/MainPage";
 import AddonPage from "./pages/addonPage/AddonPage";
+import InvalidLink from "./pages/invalidLink/InvalidLink";
 import "./App.css";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="/prototype" element={<Prototype />} />
         <Route path="/addons" element={<AddonPage />} />
+        <Route path="/invalidlink" element={<InvalidLink />} />
       </Routes>
     </div>
   );
