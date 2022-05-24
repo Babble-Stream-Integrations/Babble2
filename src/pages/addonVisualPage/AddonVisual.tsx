b4fa24add6e86f7cfa64f2b1959788fa30b31636
FinndeJong <70587533+FinndeJong@users.noreply.github.com>
2022-05-25 00:56:22 +0200

    URL check

--- src/pages/addonVisualPage/AddonVisual.tsx
+++ src/pages/addonVisualPage/AddonVisual.tsx
@@ -1,10 +1,26 @@
 import React, { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function AddonVisual() {
   const location = useLocation();
+  const navigate = useNavigate();
   useEffect(() => {
-    console.log(location.pathname, location.search);
+    if (location.search !== "") {
+      if (
+        location.search.match(/user=([^&]*)/g) &&
+        location.search.match(/user=([^&]*)/g).length === 1 &&
+        location.search.match(/addon=([^&]*)/g) &&
+        location.search.match(/addon=([^&]*)/g).length === 1
+      ) {
+        console.log("match");
+      } else {
+        navigate("../../invalidlink", { replace: true });
+        console.log("Back to start!");
+      }
+    } else {
+      navigate("../invalidlink", { replace: true });
+      console.log("Back to start!");
+    }
   }, []);
 
   return (
