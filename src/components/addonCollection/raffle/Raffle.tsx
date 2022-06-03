d7201f319e79e1bfe08e38be69caa85574914465
FinndeJong <finn.dejong@student.hu.nl>
2022-06-03 12:51:50 +0200

    Update VisualRewrite
    - Connection RTDB
    - CSS animaties
    - Css connectie met Firestore

--- src/components/addonCollection/raffle/Raffle.tsx
+++ src/components/addonCollection/raffle/Raffle.tsx
@@ -1,6 +1,9 @@
-import { useEffect } from "react";
+import { onValue, ref } from "firebase/database";
+import { rtdb } from "../../../firebase/RealtimeDatabase";
+import { useEffect, useState } from "react";
 import ticketlogo from "../../../assets/Ticket-icon.png";
 import "./Raffle.css";
+import { ChooseRaffleAnimation } from "./RaffleAnimation";
 
 interface addonTypes {
   data: object;
@@ -8,36 +11,72 @@ interface addonTypes {
 }
 
 function Raffle({ dataRecieved, data }: addonTypes) {
+  const [render, setRender] = useState(false);
+  const [raffleAnimation, setRaffleAnimation] = useState(
+    "alert-animation-left"
+  );
   useEffect(() => {
-    console.log(data["styling"]);
+    const styling = data["styling"];
     if (typeof data === "object") {
-      Object.keys(data["styling"]).forEach((x) => {
-        console.log(x);
+      for (const [x, y] of Object.entries(styling)) {
+        document
+          .getElementById("canvas")
+          .style.setProperty("--" + x, y.toString());
+        // if (x === "position") {
+        //   document
+        //     .getElementById("raffle")
+        //     .classList.add(ChooseRaffleAnimation(y.toString()));
+        // }
+      }
+      console.log(data["uniqueString  "]);
+      const eventRef = ref(rtdb, data["uniqueString"]);
+      onValue(eventRef, (snapshot) => {
+        const data = snapshot.val();
+        const eventType = data.type;
+        switch (eventType) {
+          case "start":
+            console.log("startcase");
+            document
+              .getElementById("raffle")
+              .style.setProperty("display", "block");
+            break;
+          case "end":
+            document
+              .getElementById("raffle")
+              .style.setProperty("display", "none");
+            console.log("endcase");
+            break;
+          case "idle":
+            console.log("idlecase");
+            break;
+        }
       });
     }
   }, [dataRecieved]);
   return (
-    <div className="canvas">
-      <div className="raffle">
-        <div className="raffle__container">
-          <div className="raffle__logo">
-            <img
-              src={ticketlogo}
-              alt="Ticket logo Raffle"
-              className="raffle__img"
-            />
-          </div>
-          <div className="raffle__text">
-            <div className="raffle__title">
-              Raffle <b>starting</b>!
+    <>
+      <div id="canvas" className="canvas">
+        <div id="raffle" className="raffle">
+          <div className="raffle__container">
+            <div className="raffle__logo">
+              <img
+                src={ticketlogo}
+                alt="Ticket logo Raffle"
+                className="raffle__img"
+              />
             </div>
-            <div className="raffle__subtitle">
-              Use <b>/join</b> to enter.
+            <div className="raffle__text">
+              <div className="raffle__title">
+                Raffle <b>starting</b>!
+              </div>
+              <div className="raffle__subtitle">
+                Use <b>/join</b> to enter.
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
