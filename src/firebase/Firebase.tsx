572789caf273548104d2858378289c74371f7fb4
kaischreurs <70720881+kaischreurs@users.noreply.github.com>
2022-06-10 14:45:04 +0200

    comment over de apikey
    zodat mensen stoppen met hier over klagen in reviews

--- src/firebase/Firebase.tsx
+++ src/firebase/Firebase.tsx
@@ -6,6 +6,8 @@ import {
   getToken,
 } from "firebase/app-check";
 
+// de "apikey" hier is geen security risk https://firebase.google.com/docs/projects/api-keys
+// het is verder ook net zo goed mogelijk er bij te komen via inspect element
 const firebaseConfig = {
   apiKey: "AIzaSyCzNQ2O8_KEF7rcupBT8gNjB0_BIE7K4ig",
   authDomain: "babble-d6ef3.firebaseapp.com",
