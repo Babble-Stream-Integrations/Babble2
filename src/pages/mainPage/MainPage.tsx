2744269a21c6b208eff246970488cfc506b6fd6e
kaischreurs <70720881+kaischreurs@users.noreply.github.com>
2022-06-10 10:27:52 +0200

    test ding weg

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -7,25 +7,6 @@ function MainPage() {
       <div>
         <p style={{ color: "white" }}>
           This is de index pagina van Babble streaming integrations!
-          <button
-            onClick={async () => {
-              const appchecktoken = await getAppcheck();
-              console.log(appchecktoken);
-              fetch(
-                "https://europe-west1-babble-d6ef3.cloudfunctions.net/default/",
-                {
-                  headers: {
-                    Origin: "https://dev-babble.web.app",
-                    appchecktoken: appchecktoken,
-                  },
-                }
-              ).then((response) => {
-                console.log(response);
-              });
-            }}
-          >
-            testbutton
-          </button>
         </p>
       </div>
     </div>
