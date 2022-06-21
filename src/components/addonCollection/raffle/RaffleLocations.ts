c9a9788caf09f87998289aa9deb81da2617903c6
FinndeJong <70587533+FinndeJong@users.noreply.github.com>
2022-06-21 23:42:16 +0200

    Voor JOJO gezeik
    - typing

--- src/components/addonCollection/raffle/RaffleLocations.ts
+++ src/components/addonCollection/raffle/RaffleLocations.ts
@@ -1,45 +1,51 @@
 // P stands for position and number stands for the 3x3 grid from 0-2 on both axis
-export const P00: object = {
+
+interface positionTypes {
+  justifyContent: string;
+  alignItems: string;
+}
+
+export const P00: positionTypes = {
   justifyContent: "left",
   alignItems: "flex-start",
 };
 
-export const P10: object = {
+export const P10: positionTypes = {
   justifyContent: "center",
   alignItems: "flex-start",
 };
 
-export const P20: object = {
+export const P20: positionTypes = {
   justifyContent: "right",
   alignItems: "flex-start",
 };
 
-export const P01: object = {
+export const P01: positionTypes = {
   justifyContent: "left",
   alignItems: "center",
 };
 
-export const P11: object = {
+export const P11: positionTypes = {
   justifyContent: "center",
   alignItems: "center",
 };
 
-export const P21: object = {
+export const P21: positionTypes = {
   justifyContent: "right",
   alignItems: "center",
 };
 
-export const P02: object = {
+export const P02: positionTypes = {
   justifyContent: "left",
   alignItems: "flex-end",
 };
 
-export const P12: object = {
+export const P12: positionTypes = {
   justifyContent: "center",
   alignItems: "flex-end",
 };
 
-export const P22: object = {
+export const P22: positionTypes = {
   justifyContent: "right",
   alignItems: "flex-end",
 };
