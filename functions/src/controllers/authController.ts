25890285b2cc62ee57dd80b3ddf6743e1beff886
JoJ3o <42371079+JoJ3o@users.noreply.github.com>
2022-07-05 14:55:41 +0200

    Describe Auth & Addon paths

--- functions/src/controllers/authController.ts
+++ functions/src/controllers/authController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import * as twitchAuth from "../services/twitchAuth";
 import * as youtubeAuth from "../services/youtubeAuth";
 
-export const getCode = async (req: Request, res: Response) => {
+export const getURL = async (req: Request, res: Response) => {
   const { user, platform, addonType } = req.params;
   if (addonType !== "raffleSystem" && addonType !== "automaticStreamTitle") {
     throw new Error("No valid addonType");
@@ -21,11 +21,11 @@ export const setTokens = async (req: Request, res: Response) => {
 
   if (platform === "twitch") {
     await twitchAuth.setAccessTokens(user, platform, req.body.code);
-    return res.send({ result: `${platform} tokens added to ${user}` });
+    return res.send({ result: `${platform} tokens added to ${user}!` });
   }
   if (platform === "youtube") {
     await youtubeAuth.setAccessTokens(user, platform, req.body.code);
-    return res.send({ result: `${platform} tokens added to ${user}` });
+    return res.send({ result: `${platform} tokens added to ${user}!` });
   }
   throw new Error("No valid platform");
 };
